2a43ac6cd20867d06fa14a1f1a7522a8b686ebc7
sindy silva <sindysilva520@gmail.com>
2022-04-11 00:41:01 -0500

    commit2

--- script.js
+++ script.js
@@ -3,17 +3,25 @@ let Totalpropinas = 0;
 let totalcuenta = 0;
 
 function calcular(){
-    let Valorcompra = parseInt( document.getElementById('totalC').value);
-    let porcentajes = parseInt( document.getElementById('porcen').value);
-          propinas = porcentajes /100;
-          Totalpropinas = Valorcompra * propinas ;
-         totalcuenta = Valorcompra + Totalpropinas;
-        document.getElementById('tc').innerHTML =Valorcompra;
-        document.getElementById('pr').innerHTML =Totalpropinas;
-        document.getElementById('pgr').innerHTML =totalcuenta;
+   let Valorcompra = parseInt( document.getElementById('totalC').value);
+   let porcentajes = parseInt( document.getElementById('porcen').value);
+   propinas = porcentajes /100;
+   Totalpropinas = Valorcompra * propinas ;
+   totalcuenta = Valorcompra + Totalpropinas;
+     document.getElementById('tc').innerHTML =Valorcompra;
+     document.getElementById('pr').innerHTML =Totalpropinas;
+     document.getElementById('pgr').innerHTML =totalcuenta;
+   let validado = true;
+   elementos = document.getElementsByClassName("inputFormu");
+   for(i=0;i<elementos.length;i++){
+     if(elementos[i].value == "" || elementos[i].value == null){
+     validado = false
+     }
+   }
+   if(validado){
+         
+      }else{
+   alert("Ingrese los campos vacio")   
+   }
+  
 }
-
-
-
-
-
