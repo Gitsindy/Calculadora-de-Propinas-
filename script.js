5f72ecd29c8a0f23020909641cf6e86d564564bc
sindy silva <sindysilva520@gmail.com>
2022-04-11 14:42:15 -0500

    validacion2

--- script.js
+++ script.js
@@ -2,26 +2,28 @@ let propinas = 0;
 let Totalpropinas = 0;
 let totalcuenta = 0;
 
-function calcular(){
-   let Valorcompra = parseInt( document.getElementById('totalC').value);
-   let porcentajes = parseInt( document.getElementById('porcen').value);
-   propinas = porcentajes /100;
-   Totalpropinas = Valorcompra * propinas ;
-   totalcuenta = Valorcompra + Totalpropinas;
-     document.getElementById('tc').innerHTML =Valorcompra;
-     document.getElementById('pr').innerHTML =Totalpropinas;
-     document.getElementById('pgr').innerHTML =totalcuenta;
-   let validado = true;
-   elementos = document.getElementsByClassName("inputFormu");
-   for(i=0;i<elementos.length;i++){
-     if(elementos[i].value == "" || elementos[i].value == null){
-     validado = false
-     }
-   }
-   if(validado){
-         
-      }else{
-   alert("Ingrese los campos vacio")   
-   }
-  
+function calcular() {
+  let Valorcompra = parseInt(document.getElementById('totalC').value);
+  let porcentajes = parseInt(document.getElementById('porcen').value);
+  let validado = true;
+  elementos = document.getElementsByClassName("inputFormu");
+  for (i = 0; i < elementos.length; i++) {
+    if (elementos[i].value == "" || elementos[i].value == null) {
+      validado = false
+    }
+    if (elementos[i].value < 0) {
+      return false
+    }
+  }
+  if (validado) {
+    propinas = porcentajes / 100;
+    Totalpropinas = Valorcompra * propinas;
+    totalcuenta = Valorcompra + Totalpropinas;
+    document.getElementById('tc').innerHTML = Valorcompra;
+    document.getElementById('pr').innerHTML = Totalpropinas;
+    document.getElementById('pgr').innerHTML = totalcuenta;
+  } else {
+    alert("Ingrese los campos vacio")
+  }
+
 }
